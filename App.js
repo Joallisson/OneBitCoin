56a618af82119b047d45d52b9e4edeea879072df
Joallisson <joallissons@gmail.com>
2022-01-13 21:26:58 -0300

    Iniciando lógica

--- App.js
+++ App.js
@@ -6,6 +6,24 @@ import CurrentPrice from './src/components/CurrentPrice';
 import QuotationsList from './src/components/QuotationsList';
 import QuotationsItems from './src/components/QuotationsList/QuotationsItems';
 
+function addZero(number){
+  if (number <= 9) {
+    return "0" + number
+  }
+  return number
+}
+
+function url(qtdDays){
+  const date = new Date()
+  const listLastDays = qtdDays
+  const end_date = `${date.getFullYear}-${addZero(date.getMonth() + 1)}-${addZero(date.getDay())}`;
+  
+  date.setDay(date.getDay() - listLastDays)
+  
+  const start_date = `${date.getFullYear}-${addZero(date.getMonth() + 1)}-${addZero(date.getDay())}`;
+  
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&${end_date}`
+}
 
 export default function App() {
   return (
