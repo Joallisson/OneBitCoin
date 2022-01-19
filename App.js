4edf7837a2595bb94bd014a117f9901db4062a8f
Joallisson <joallissons@gmail.com>
2022-01-18 22:54:45 -0300

    Adicionando os ícones
    Mas ainda tem o erro do preço atual que precisa ser resolvido

--- App.js
+++ App.js
@@ -55,10 +55,15 @@ export default function App() {
   const [coinsGraphicList, setCoinsGraphicList] = useState([0])
   const [days, setDays] = useState(30)
   const [updateData, setUpdateData] = useState(true)
+  const [price, setPrice] = useState()
 
   function updateDay(number){
     setDays(number)
     setUpdateData(true)
+  }
+
+  function priceCotation(){
+    setPrice(coinsGraphicList.pop())
   }
 
   useEffect(() => { //ATUALIZA A LISTA DE BITCOINS E O GRÁFICO SEMPRE QUE O USUÁRIO ESCOLHE UM FILTRO
@@ -89,7 +94,7 @@ export default function App() {
       barStyle = 'light-content'
       />
 
-      <CurrentPrice/>
+      <CurrentPrice lastCotation={price}/>
       <HistoryGraphic infoDataGraphic={coinsGraphicList}/>
       <QuotationsList filterDay={updateDay} listTransactions={coinsList} />
 
--- src/components/CurrentPrice/index.js
+++ src/components/CurrentPrice/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import {View, Text} from 'react-native'
 import styles from './styles'
 
-export default function CurrentPrice(){
+export default function CurrentPrice(props){
     return(
         <View style = {styles.headerPrice}>
-            <Text style = {styles.currentPrice}>$ 54423.355</Text>
+            <Text style = {styles.currentPrice}>$ {props.lastCotation}</Text>
             <Text style = {styles.textPrice}>Última cotação</Text>
         </View>
     )
